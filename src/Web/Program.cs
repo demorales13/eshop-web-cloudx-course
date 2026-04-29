1c54a3bc81cb8a133c935de6e3c3e12a2ebc1720
damorales1-clgx <damorales@corelogic.com>
2026-04-29 13:21:28 -0500

    Add Application Insights telemetry integration
    Added Microsoft.ApplicationInsights.AspNetCore and Microsoft.Extensions.Logging.ApplicationInsights packages. Registered Application Insights telemetry in Program.cs to enable monitoring and logging.

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -15,6 +15,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add service defaults & Aspire components.
 builder.AddAspireServiceDefaults();
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 builder.Services.AddDatabaseContexts(builder.Environment, builder.Configuration);
 
 builder.Services.AddCookieSettings();
